f758dcfdc8e60d45a72163ad69427b0da5ac4950
Rich Wickens <rwickens100@gmail.com>
2024-06-29 12:26:23 +0100

    basic app complete

--- healthmetrics/metrics/admin.py
+++ healthmetrics/metrics/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Smoking, Diet, Weight, Activity, ResistanceTraining, CoffeeDrinking
 
-# Register your models here.
+admin.site.register(Smoking)
+admin.site.register(Diet)
+admin.site.register(Weight)
+admin.site.register(Activity)
+admin.site.register(ResistanceTraining)
+admin.site.register(CoffeeDrinking)
